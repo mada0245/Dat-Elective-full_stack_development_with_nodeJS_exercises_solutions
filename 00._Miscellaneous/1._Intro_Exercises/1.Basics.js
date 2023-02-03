17d231788e2972b92b80014972c4bb1ccb0f8c99
Anders Latif <andl@kea.dk>
2023-02-03 12:56:40 +0100

    Solved 1.Basics and added 2.Strings

--- 00._Miscellaneous/1._Intro_Exercises/1.Basics.js
+++ 00._Miscellaneous/1._Intro_Exercises/1.Basics.js
@@ -8,17 +8,30 @@ const lastName = "Latif";
 // EXERCISE
 // show in the console
 // My first name is Anders and my last name is Latif
-
+// console.log("My first name is " + firstName + " and my last name is " + lastName);
+// console.log(`My first name is ${firstName} and my last name is ${lastName}`);
 
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
 
-const year = "2021";
+const year = "2022";
 const number = 1;
 
 // Add the year plus the number
-// The result should be 2020
+// The result should be 2023
 // You cannot touch line 1 or 2
+
+const resultOne = Number(year) + number;
+const resultTwo = parseInt(year) + number;
+const resultThree = +year + number;
+
+
+// console.log(resultThree);
+
+console.log(Number("2343dsfdsfds"));
+console.log(parseInt("2343dsfdsfds"));
+
 
 
 // --------------------------------------
+
